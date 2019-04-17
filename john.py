baa147f8fbddc87d3b6cec1f4caae1e3e7fb3e0e
L <lucas@localhost.localdomain>
2019-04-17 15:20:43 -0300

    agrego mas cosas

--- john.py
+++ john.py
@@ -1,5 +1,5 @@
 import subprocess
 
-run = subprocess.check_output('john  --max-length:6 --incremental:lower pass.lst',shell=True)
+run = subprocess.check_output('john  --max-length:5 --incremental:lower  pass.lst --format:Raw-SHA1 --node:2/2',shell=True)
 print('----------------')
 print('status '+ run.decode())
